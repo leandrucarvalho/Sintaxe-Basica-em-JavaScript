6400cd1549e6858ed2420aa424a9585b09669d77
leandrucarvalho <leandrucs@gmail.com>
2022-03-16 22:17:21 -0300

    Add um comparador de numeros feito em js

---
+++ sintaxe-operadores-JavaScript/README.md
@@ -0,0 +1,20 @@
+# Sintaxe e Operadores
+
+Este repositório contém uma atividade prática do Curso "Sintaxe e Operadores", que faz parte do bootcamp "Impulso React Web Developer" da [Digital Innovation One](https://www.dio.me/).
+
+## Proposta da atividade
+
+- Crie uma função que recebe dois números como parâmetros.
+- Confira se os números são iguais.
+- Confira se a soma dos números é maior que 10 ou menor que 20.
+- Retorne uma string dizendo "Os números `num1` e `num2` não/são iguais. Sua soma é `soma`, que é `maior/menor` que 10 e `maior/menor` que 20".
+
+**Exemplo da atividade:**
+
+```
+Input: 1, 2
+Output: Os números 1 e 2 não são iguais. Sua soma é 3, que é menor que 10 e menor que 20.
+```
+Observação: Coloquei duas validações para poder aceitar o número '0' e outra validação para digitar apenas números.
+**Print do resultado da minha atividade:**
+
---
+++ sintaxe-operadores-JavaScript/comparaNumeros.js
@@ -0,0 +1,54 @@
+function comparaNumeros(num1, num2) {
+
+    if (typeof (num1) == "undefined" || typeof (num2) == "undefined") {
+        console.log('Numeros requeridos');
+        return;
+    }
+
+    if (typeof num1 != 'number') {
+        console.log('Numeros 1 precisa ser um numero');
+        return;
+    }
+
+    if (typeof num2 != 'number') {
+        console.log('Numeros 2 precisa ser um numero');
+        return;
+    }
+
+    let primeiraFrase = criaPrimeiraFrase(num1, num2);
+    let segundaFrase = criaSegundaFrase(num1, num2);
+
+    return `${primeiraFrase} ${segundaFrase}`;
+}
+
+
+
+function criaPrimeiraFrase(num1, num2) {
+    let saoIguais = '';
+    if (num1 !== num2) {
+        saoIguais = 'não';
+    }
+    return `Os números ${num1} e ${num2} ${saoIguais} são iguais.`
+}
+
+function criaSegundaFrase(num1, num2) {
+    const soma = num1 + num2;
+    let resultado10 = 'menor';
+    let resultado20 = 'menor';
+
+    const compara10 = soma > 10;
+    const compara20 = soma > 20;
+
+    if (compara10) {
+        resultado10 = 'maior'
+    }
+
+    if (compara20) {
+        resultado20 = 'maior'
+    }
+
+    return `Sua soma é ${soma}, que é ${resultado10} que 10 e ${resultado20} que 20.`
+}
+
+var result = comparaNumeros(0,0);
+console.log(result);
